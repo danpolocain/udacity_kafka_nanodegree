a500fb19f9ff47a6deebd0cc54e5b6c90b8c42a5
Dan Polo Cain <danpolocain@gmail.com>
2020-01-12 23:25:04 -0300

    fix consumer

--- project01/consumers/models/lines.py
+++ project01/consumers/models/lines.py
@@ -19,9 +19,10 @@ class Lines:
 
     def process_message(self, message):
         """Processes a station message"""
-        if "org.chicago.cta.station" in message.topic():
+        
+        if "com.company.station" in message.topic():
             value = message.value()
-            if message.topic() == "org.chicago.cta.stations.table.v1":
+            if message.topic() == "com.company.stations.table":
                 value = json.loads(value)
             if value["line"] == "green":
                 self.green_line.process_message(message)
